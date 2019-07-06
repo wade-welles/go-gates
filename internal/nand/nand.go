4db3c794b91ac48cb14cd7731abd946596f498d1
Emanuel Freitas <efreitasn@hotmail.com>
2019-07-06 08:48:45 -0300

    Fix typos

--- internal/nand/nand.go
+++ internal/nand/nand.go
@@ -1,6 +1,7 @@
+// Package nand implements the NAND gate and other gates based upon on it.
 package nand
 
-// Exec performs a NAND operation on two inputs
+// Exec performs a NAND operation on two inputs.
 func Exec(a bool, b bool) bool {
 	return !(a && b)
 }
--- internal/nand/not.go
+++ internal/nand/not.go
@@ -1,6 +1,6 @@
 package nand
 
-// Not performs a NOT operation on the input
+// Not performs a NOT operation on the input.
 func Not(a bool) bool {
 	return Exec(a, a)
 }
